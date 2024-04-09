533223f043f02df65208d439a509551aaf8788dc
ViciJr <vcalafellbauza@gmail.com>
2024-04-09 03:44:22 +0200

    Update

---
+++ src/SolucioInformatica/gui/Timer/Timer.java
@@ -0,0 +1,49 @@
+package SolucioInformatica.gui.Timer;
+
+import processing.core.PApplet;
+
+public class Timer {
+
+    int numSecondsToChange;
+    int numSeconds;
+    int lastTime;
+    boolean started;
+
+    public Timer(PApplet p5, int ns){
+        this.numSecondsToChange = ns;
+        this.numSeconds = 0;
+        this.lastTime = p5.millis();
+        this.started = false;
+    }
+
+    public int getNumSeconds(){
+        return  this.numSeconds;
+    }
+
+    public void start(PApplet p5){
+        this.lastTime = p5.millis();
+        this.started = true;
+    }
+
+    public void update(PApplet p5){
+        if(started) {
+            int now = p5.millis();
+            numSeconds = (now - lastTime) / 1000;
+            if (timeOver()) {
+                resetTimer(p5);
+            }
+        }
+    }
+
+    public void setStarted(boolean b){
+        this.started = b;
+    }
+
+    public void resetTimer(PApplet p5){
+        lastTime = p5.millis();
+    }
+
+    public boolean timeOver(){
+        return (numSeconds>= numSecondsToChange);
+    }
+}
---
+++ src/SolucioInformatica/gui/Timer/TimerTest.java
@@ -0,0 +1,81 @@
+package SolucioInformatica.gui.Timer;
+
+
+
+        import SolucioInformatica.gui.Button;
+        import processing.core.PApplet;
+
+public class TimerTest extends PApplet {
+
+    // Elements de la Interfície Gràfica (Timer)
+    Timer t;
+
+    Button b;
+
+    // Color de fons
+    int bgColor = color(0);
+
+    public static void main(String[] args) {
+        PApplet.main("SolucioInformatica.gui.Timer.TimerTest", args);
+    }
+
+    public void settings(){
+        size(800, 800);        // Pantalla HD
+        smooth(10);
+    }
+
+    public void setup(){
+        // Timer de 10 segons
+        t = new Timer(this,10);
+
+        b = new Button(this, "START", 10, 10, 100, 50);
+    }
+
+    public void draw(){
+
+        background(bgColor);
+
+        // Actualitza el color de fons
+        updateColor();
+
+        fill(255); textSize(64); textAlign(CENTER);
+        text(t.getNumSeconds(), width/2, height/2);
+
+        b.display(this);
+
+    }
+
+    void updateColor(){
+
+        // Si s'ha esgotat el timer, canviam el color
+        if(t.timeOver()){
+            bgColor = color(random(255), random(255), random(255));
+        }
+
+        // Actualitza el timer
+        t.update(this);
+    }
+
+
+    // ******************* KEYBOARD interaction ***************************** //
+
+    public void keyPressed(){
+    }
+
+    // ******************* MOUSE interaction ***************************** //
+
+    public void mousePressed(){
+        if(b.mouseOverButton(this)){
+            t.start(this);
+        }
+    }
+
+    public void mouseDragged(){
+        println("MOUSE DRAGGED");
+    }
+
+    public void mouseReleased() {
+        println("MOUSE RELEASED");
+    }
+
+}
